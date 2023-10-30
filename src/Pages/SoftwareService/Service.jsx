c7fa52f759a6fedf10dcaede608f5e8914dcb05e
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-10-30 20:33:02 +0600

    company uses Technology are added

--- src/Pages/SoftwareService/Service.jsx
+++ src/Pages/SoftwareService/Service.jsx
@@ -1,6 +1,7 @@
 import KnowMore from "./KnowMore";
 import ProjectBrand from "./ProjectBrand";
 import SoftwareTeam from "./SoftwareTeam";
+import Technology from "./Technology";
 
 const Service = () => {
     return (
@@ -8,6 +9,7 @@ const Service = () => {
           <SoftwareTeam></SoftwareTeam>
           <ProjectBrand></ProjectBrand>
           <KnowMore></KnowMore>
+          <Technology></Technology>
         </div>
     );
 };
--- src/Pages/SoftwareService/Technology.jsx
+++ src/Pages/SoftwareService/Technology.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 
 const Technology = () => {
+const [images,setImages] = useState([])
+
+useEffect(()=>{
+    fetch('TechnologyData.json')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        setImages(data)
+    })
+
+},[])
+
+
+
     return (
-        <div>
+        <div >
             {/* here show the technology img which using to develop site */}
 
-            <img src="http://nxtide.com/wp-content/uploads/2021/02/dotnet.png" alt="" />
+            <h2 className="px-12 mt-16 text-4xl text-center">Our teams are skilled in a wide variety of technology stacks</h2>
+
+            <div className="px-24 py-16 grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-6">
+                {
+                    images.map((item,index)=> <div key={index}>
+
+                    <img src={item.img} alt="" />
+                    </div>)
+                }
+            </div>
             
         </div>
     );
