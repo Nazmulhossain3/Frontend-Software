e79e2195418a157dcd3939322ca64696eaf146a4
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-10-30 18:27:37 +0600

    ProjectBrand is done

--- src/Pages/SoftwareService/Service.jsx
+++ src/Pages/SoftwareService/Service.jsx
@@ -1,9 +1,11 @@
+import ProjectBrand from "./ProjectBrand";
 import SoftwareTeam from "./SoftwareTeam";
 
 const Service = () => {
     return (
         <div>
           <SoftwareTeam></SoftwareTeam>
+          <ProjectBrand></ProjectBrand>
         </div>
     );
 };
